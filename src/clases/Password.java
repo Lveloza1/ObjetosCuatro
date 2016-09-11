3198cc391dc5c522f3228a00216324250c456a9a
Luis Veloza <velozacampo31@gmail.com>
2016-09-11 16:57:55 -0500

    Ejercicio Completo

--- src/clases/Password.java
+++ src/clases/Password.java
@@ -39,35 +39,36 @@ public class Password {
 
     public String CambiarContraseña() {
         String aux = null;
-        int res, sw; 
+        int res, sw, sw1;
         boolean au = true;
-        do{
-            sw=1;
-            
-        try {
-            aux = JOptionPane.showInputDialog("Digite una nueva contraseña");
-        
-        if (aux.length() < 6) {
+        do {
+            sw = 1;
 
-            JOptionPane.showMessageDialog(null, "Contraseña débil, intente de nuevo");
+            try {
+                aux = JOptionPane.showInputDialog("Digite una nueva contraseña");
 
-        } else {
-            JOptionPane.showMessageDialog(null, "Nueva contraseña guardada");
-            this.contraseña = aux;
-        }
-        } catch (NullPointerException e) {
-            res = JOptionPane.showConfirmDialog(null, "¿Desea Abandonar?", "Salir", JOptionPane.YES_NO_OPTION);
-            if (res == 0) {
-                au = false;
-                sw =1;
+                if (aux.length() < 6) {
+
+                    JOptionPane.showMessageDialog(null, "Contraseña débil, intente de nuevo");
+                    sw1 = 0;
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Nueva contraseña guardada");
+                    this.contraseña = aux;
+                    sw1 = 1;
+                }
+
+            } catch (NullPointerException e) {
+                res = JOptionPane.showConfirmDialog(null, "¿Desea Abandonar?", "Salir", JOptionPane.YES_NO_OPTION);
+                if (res == 0) {
+                    au = false;
+                    sw = 1;
+                } else {
+                    sw = 0;
+                }
             }
-            else{
-                sw=0;
-            }
-        }
-        }while (sw==0);
+        } while (sw == 0);
 
-        
         return this.contraseña;
 
     }
